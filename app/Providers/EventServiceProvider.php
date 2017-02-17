42c79e57389ea7eff5676bbe29525cc29c402cff
Azri Ismail <azri.ismail@nazrol.tech>
2017-02-17 12:08:59 +0800

    add error handler

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Event;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Validation\ValidationException;
+use Tymon\JWTAuth\Exceptions\JWTException;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -27,6 +29,16 @@ class EventServiceProvider extends ServiceProvider
     {
         parent::boot();
 
-        //
+        app('Dingo\Api\Exception\Handler')->register(function (ModelNotFoundException $exception) {
+            return \Response::make(['status' => 'error', 'message' => $exception->getMessage()], 404);
+        });
+
+        app('Dingo\Api\Exception\Handler')->register(function (ValidationException $exception) {
+            return \Response::make(['status' => 'error', 'message' => 'Validation failed!', 'errors' => $exception->validator->getMessageBag()->toArray()], 400);
+        });
+
+        app('Dingo\Api\Exception\Handler')->register(function (JWTException $exception) {
+            return \Response::make(['status' => 'error', 'message' => $exception->getMessage()], 401);
+        });
     }
 }
